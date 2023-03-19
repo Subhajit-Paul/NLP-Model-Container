06107fbdd9f428de43a334d1a2aa0ba408799d81
Subhajit Paul <paulsubhajit78242548@gmail.com>
2023-03-20 03:17:19 +0530

    Changes to be committed: 	modified:   model.py     Added Label Words

--- model.py
+++ model.py
@@ -6,10 +6,27 @@ feature = joblib.load(open("feature.pkl", 'rb'))
 
 app = FastAPI()
 
+label = {
+    0:  "Admiration/Pride",
+    1:  "Amusement",
+    2:  "Love",
+    3:  "Desire/Optimism",
+    4:  "Caring",
+    5:  "Gratitude/Relief",
+    6:  "Approval/Realization",
+    7:  "Surprise/Curiosity/Confusion",
+    8:  "Fear/Nervousness",
+    9:  "Remorse/Embarrassment",
+    10: "Disappointment/Sadness/Grief",
+    11: "Annoyance/Anger/Disgust",
+    12: "Disapproval",
+    13: "Exitement/Joy"
+}
+
 @app.post("/req")
 async def login(data: str):
     results = model.predict(feature.transform([data.lower()]))
-    return {"predictions": f"{results[0]}"}
+    return {"predictions": f"{label[results[0]]}"}
     return data
 
 
